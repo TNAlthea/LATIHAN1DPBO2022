46dacc77bca7138293fdf93c2a49118af7a84547
Sabian A <99664611+TNAlthea@users.noreply.github.com>
2022-02-14 21:08:22 +0700

    Create TimSepakBola.php

---
+++ LATIHAN B/PHP/TimSepakBola.php
@@ -0,0 +1,59 @@
+<?php
+
+class TimSepakBola
+{
+    private $NamaTim = "Lorem Ipsum";
+    private $AsalNegaraTim = "Indonesia";
+    private $NamaPemain = "Ipsum Lorem";
+    private $TahunBerdiri = 1;
+
+    public function __construct($NamaTim = "Lorem Ipsum", $AsalNegaraTim = "Indonesia", $NamaPemain = "Ipsum Lorem", $TahunBerdiri = 2000){
+        $this->NamaTim = $NamaTim;
+        $this->AsalNegaraTim = $AsalNegaraTim;
+        $this->NamaPemain = $NamaPemain;
+        $this->TahunBerdiri = $TahunBerdiri;
+    }
+
+    public function setNamaTim($NamaTim){
+        $this->NamaTim = $NamaTim;
+    }
+
+    public function getNamaTim(){
+        return $this->NamaTim;
+    }
+
+    public function setAsalNegara($AsalNegaraTim){
+        $this->AsalNegaraTim = $AsalNegaraTim;
+    }
+
+    public function getAsalNegara(){
+        return $this->AsalNegaraTim;
+    }
+
+    public function setNamaPemain($NamaPemain){
+        $this->NamaPemain = $NamaPemain;
+    }
+
+    public function getNamaPemain(){
+        return $this->NamaPemain;
+    }
+
+    public function setTahunBerdiri($TahunBerdiri){
+        $this->TahunBerdiri = $TahunBerdiri;
+    }
+
+    public function getTahunBerdiri(){
+        return $this->TahunBerdiri;
+    }
+
+}
+
+function tampilData($TimSepakBola){
+    echo "Data Tim Sepak Bola" ."<br>";
+    echo "Nama Tim: " .$TimSepakBola->getNamaTim() ."<br>";
+    echo "Asal Negara Tim: " .$TimSepakBola->getAsalNegara() ."<br>";
+    echo "Nama Pemain: " .$TimSepakBola->getNamaPemain() ."<br>";
+    echo "Tahun Tim Didirikan: " .$TimSepakBola->getTahunBerdiri() ."<br> ======== <br>";
+}
+
+?>
