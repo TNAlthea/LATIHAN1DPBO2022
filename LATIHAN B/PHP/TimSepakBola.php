be209b3eb2c957fd0dc240a1f8b53c7a82f4d311
Sabian A <99664611+TNAlthea@users.noreply.github.com>
2022-02-14 21:09:55 +0700

    Update TimSepakBola.php

--- LATIHAN B/PHP/TimSepakBola.php
+++ LATIHAN B/PHP/TimSepakBola.php
@@ -2,18 +2,21 @@
 
 class TimSepakBola
 {
+    //atribut privat
     private $NamaTim = "Lorem Ipsum";
     private $AsalNegaraTim = "Indonesia";
     private $NamaPemain = "Ipsum Lorem";
     private $TahunBerdiri = 1;
-
+    
+    //construct
     public function __construct($NamaTim = "Lorem Ipsum", $AsalNegaraTim = "Indonesia", $NamaPemain = "Ipsum Lorem", $TahunBerdiri = 2000){
         $this->NamaTim = $NamaTim;
         $this->AsalNegaraTim = $AsalNegaraTim;
         $this->NamaPemain = $NamaPemain;
         $this->TahunBerdiri = $TahunBerdiri;
     }
-
+    
+    /*get set*/
     public function setNamaTim($NamaTim){
         $this->NamaTim = $NamaTim;
     }
@@ -48,6 +51,7 @@ class TimSepakBola
 
 }
 
+//fungsi untuk menampilkan data
 function tampilData($TimSepakBola){
     echo "Data Tim Sepak Bola" ."<br>";
     echo "Nama Tim: " .$TimSepakBola->getNamaTim() ."<br>";
